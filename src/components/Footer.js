81c6d913deab81ba036468fd6a128bd66446adba
jai570 <sharmajaikishan60@gmail.com>
2021-12-05 16:10:14 +0530

    dictionary api

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+          Roadside Coder
+        </a>
+      </span>
+      <div className="iconContainer">
+        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+          <i className="fab fa-youtube fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
